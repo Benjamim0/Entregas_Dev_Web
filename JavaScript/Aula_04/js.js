931dbb821ee0dbd57f88bae32b5749a88a7c963e
Benjamim0 <Benjamimrodrigues64@gmail.com>
2023-10-11 21:25:12 -0300

    att

---
+++ JavaScript/Aula_04/js.js
@@ -0,0 +1,33 @@
+// let mediaAluno = 9.5;
+
+
+// if (mediaAluno >= 7){
+// console.log(`o aluno foi aprovado`); 
+// } else {
+//     console.log(`O aluno foi reprovado`);
+// }
+
+// let mediaAluno = 10;
+
+// if (mediaAluno >=9) {
+// console.log(`Aluno aprovado`);
+// } else (mediaAluno >=7); {
+// console.log(`Aluno reprovado`);
+// }
+
+// let temSol = true
+// let dinheiro = 1200
+// if (temSol == !dinheiro >= 1000 ) {
+//     console.log(`vou viajar`);
+// } else {
+//     console.log(`não vou viajar`);
+// }
+
+switch (key) {
+case value:
+    
+    break;
+
+    default:
+        break;
+}
