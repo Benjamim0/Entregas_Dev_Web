e6031cd04a226b522d6b07269408ffc05add2aee
Benjamim <benjamimrodrigues64@gmail.com>
2023-10-16 16:33:58 -0300

    att

---
+++ JavaScript/Aula_03/lição_Rafa/dcript.js
@@ -0,0 +1,25 @@
+let fruta01 = "Uva"
+let fruta02 = "Pera"
+let fruta03 = "Carambola"
+let fruta04 = "Banana"
+let fruta05 = "  Melão"
+
+console.log(fruta01 [1])
+
+console.log(fruta01.length)
+
+console.log(fruta01.toUpperCase())
+console.log(fruta02.toLowerCase())
+
+console.log(fruta01.substring(0,3));
+
+let frase= 'A vida é muito boa'
+let oracao = frase.split(' ')
+console.log(oracao [1]);
+
+let variavel = 'Conheça nossos produtos'
+let novavariavel = variavel.replace('conheça', 'produtos') 
+console.log(novavariavel);
+
+let texto = '   Jesus te ama   '.trim()
+console.log(texto);
