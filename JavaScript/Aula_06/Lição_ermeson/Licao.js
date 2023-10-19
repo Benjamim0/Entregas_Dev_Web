ccf28f2ec78694d44051b855dfa2b47c29ca2e44
Benjamim0 <Benjamimrodrigues64@gmail.com>
2023-10-18 21:26:47 -0300

    att

---
+++ JavaScript/Aula_06/Lição_ermeson/Licao.js
@@ -0,0 +1,14 @@
+let times = ["São Paulo","Cruzeiro","Vasco","Arsenal","Real Madrid"]
+
+let time01 = times.pop()
+console.log(time01);
+console.log(times); 
+console.log(times.length)
+
+times.shift()
+console.log(times); 
+console.log(times.length)
+
+times.unshift('Barcelona')
+console.log(times);
+
