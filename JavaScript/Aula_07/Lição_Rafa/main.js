a40d47dc7299e0e94ee3205de9b20bfee8bba5b1
Benjamim0 <Benjamimrodrigues64@gmail.com>
2023-10-19 21:47:06 -0300

    att

--- JavaScript/Aula_07/Lição_Rafa/main.js
+++ JavaScript/Aula_07/Lição_Rafa/main.js
@@ -1,3 +1,17 @@
-let alunos = {
+let media = 7 
+let alunos = [
+    {
+        nome:"Benjamim" ,
+        sobrenome:"Rodrigues",
+        idade:"17",
+        notas: "10, 6, 8, 8",
+        turma:"3° A",
+        horarioDeChegada:"12h50" ,
+        aulas:"Português, Matemática, História",
+        horarioDeSaida:"18h30",
+       
+    }
+]
+
     
-}
+
