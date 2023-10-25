cd1b34731613d13900d2c9faceae3554dd7dc18b
Benjamim <benjamimrodrigues64@gmail.com>
2023-10-25 15:06:05 -0300

    att

--- JavaScript/Aula_08/ermesonLiçao/main.js
+++ JavaScript/Aula_08/ermesonLiçao/main.js
@@ -6,49 +6,39 @@ let calculoIMC = (peso, altura) => {
     return peso / (altura * altura)
 }
 
-console.log(`o seu IMC é:${calculoIMC(kg, cm).toFixed(2)}`);
+console.log(`O seu IMC é:${calculoIMC(kg, cm).toFixed(2)}`);
 
 if (calculoIMC(kg, cm) < 16,9) {
-        console.log(`Você está muito abaixo do peso.`);
+        console.log(`Muito abaixo do peso.`);
     }else 
     if (17 < calculoIMC(kg,cm) < 18,4) {
-        console.log(`Você está abaixo do peso.`);
-
+        console.log(`Abaixo do peso.`);
     }else 
     if (18 < calculoIMC(kg,cm) < 24,9) {
-        console.log(`Você está no peso normal.`);
-
+        console.log(`Peso normal.`);
     }else 
     if (25 < calculoIMC(kg,cm) < 29,9) {
-        console.log(`Você está acima do peso.`);
-
+        console.log(`Acima do peso.`);
     }else 
     if (30 < calculoIMC(kg,cm) < 34,9) {
-        console.log(`Você está com Obesidade grau I.`);
-
+        console.log(`Obesidade grau I.`);
     }else 
     if (35 < calculoIMC(kg,cm) < 40) {
-        console.log(`Você está com Obesidade grau II.`);
-
+        console.log(`Obesidade grau II.`);
     }else 
     if (calculoIMC(kg,cm) > 40) {
         console.log(`Obesidade grau III.`);
     }else{
-        console.log(`Insira valores válidos!`);
-
+        console.log(`Coloque algo válido!`);
 }
-
 // EURO PRA REAL
 let reais = 100
 
 function conversao (real) {
     return 0.19 * real
 }
-console.log(`Se você possui ${reais} reais isso equivale a ${conversao(reais).toFixed(1)} euros`);
-
-
+console.log(`Seus ${reais} reais equivalem a ${conversao(reais).toFixed(1)} euros`);
 // BOTÃO RETORNÁVEL
-
 const clique = () => {
     console.log(`Soma `);
 }
