73069fc4e30512f4d2225dc6b342c1dd0d6b01b7
Benjamim0 <Benjamimrodrigues64@gmail.com>
2023-10-24 21:43:23 -0300

    att

---
+++ JavaScript/Aula_09/main02.js
@@ -0,0 +1,14 @@
+let nomefuncionarios = [
+    {nome: 'Wally', salario: 4800},
+    
+    {nome: 'Matheus', salario: 2100},
+    
+    {nome: 'Marcos', salario: 1800},
+    
+    {nome: 'Lucas', salario: 7100},
+    
+    {nome: 'João', salario: 15000}
+]
+nomefuncionarios.forEach((funcionarios) => {
+ return(console.log)
+})
