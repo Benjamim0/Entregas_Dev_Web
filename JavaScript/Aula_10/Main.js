78e03a2a773432523ef0ef3c1bad56091fe6cd91
Benjamim0 <Benjamimrodrigues64@gmail.com>
2023-10-25 21:48:25 -0300

    att

---
+++ JavaScript/Aula_10/Main.js
@@ -0,0 +1,30 @@
+// let aluno ={
+//     ra: 92723 ,
+//     nome: "Tarso",
+//     idade: 17,
+//     curso: "dev-Web"
+// }
+
+// class alunosIOS {
+//     constructor(ra ,nome ,idade) {
+//     this.ra = ra 
+//     this.nome =nome 
+//     this.idade = idade
+// }
+// }
+
+
+// let aluno01 = new alunosIOS(912, "Bembem", 541)
+ 
+class carros {
+    constructor(marca , modelo , valor ,ano ,cor){
+    this.marca = marca ,
+    this.modelo = modelo ,
+    this.valor = valor ,
+    this.ano = ano ,
+    this.cor = cor
+    }
+}
+let carro1 = new carros ("lamborghini" ,"huracan" ,"muito dinheiro", 2020 ,"preto")
+let carro2 = new carros ("lamborghini" ,"diablo" ,"muito dinheiro", 2019 ,"preto")
+let carro3 = new carros ("lamborghini" ,"spider" ,"muito dinheiro", 2019 ,"preto")
