d9957ee1edd8c18e9c4649d1dfe9bc0cb2279f1e
Benjamim <benjamimrodrigues64@gmail.com>
2023-11-23 15:52:13 -0300

    ex de react

---
+++ REACT/exercicio-aula04/Aula_04/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+const estilo = { fontSize: '12px'  }
+
+const Footer = () => {
+  return (
+    <div>
+        <p style={ estilo }>&copy;Benjamim</p>
+        <p>email: benjamimrodrigues64@gmail.com</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ REACT/exercicio-aula04/Aula_04/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+const estilo = { 
+    color: 'blue'
+  }
+  const Header = () => {
+    return (
+      <div>
+          <section>
+              <h1 style={estilo}>Benjamim</h1>
+              <p>Lorem ipsum dolor sit amet.</p>
+          </section>
+      </div>
+    )
+  }
+  
+  export default Header
